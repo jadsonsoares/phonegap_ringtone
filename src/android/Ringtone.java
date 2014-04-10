83b01463530e1f95b93bc16162371aa607f00ff0
jadson <jadson.soares@gmail.com>
2014-04-10 11:15:06 -0300

    comit 6
    versão final do android

--- src/android/Ringtone.java
+++ src/android/Ringtone.java
@@ -4,23 +4,56 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.Settings;
 
 public class ringtone extends CordovaPlugin {
 	
 	public static final String ACTION_OPEN_SELECT_RINGTONE = "selectRingtone";
 	
+	private static final int RINGTONE_PICKED = 1;
+	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		
+		//loadData();
+		
 		try {
+			
 		    if (ACTION_OPEN_SELECT_RINGTONE.equals(action)) {
+		    	System.err.println(">>>>>>>>>>>>>>>>>>>>SUCESS>>>>>>>>>>>>>>>>>");
 		    	
-		    	callbackContext.error(">>>>>>>>>>>>>>>SUCCESS!!>>>>>>>>>>>>>>>>>>>>");
-		       return true;
+		    	Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+		        // Allow user to pick 'Default'
+		        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
+		        // Show only ringtones
+		        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_RINGTONE);
+		        // Don't show 'Silent'
+		        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
+		        
+		        Uri ringtoneUri;
+		        ringtoneUri = RingtoneManager.getActualDefaultRingtoneUri(this.cordova.getActivity().getBaseContext(), RingtoneManager.TYPE_RINGTONE);
+//		        System.err.println(">>>>>>>>>>>>>>>>>>>>mCustomRingtone: " +ringtoneUri.toString());
+		        
+		        if (ringtoneUri.toString() == null){
+		            // Otherwise pick default ringtone Uri so that something is selected.
+		            ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+		        }
+		        
+//		        System.err.println(">>>>>>>>>>>>>>>>>>>>RINGTONE: " +ringtoneUri);
+		        
+		        // Put checkmark next to the current ringtone for this contact
+		        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, ringtoneUri);
+		        
+		        // Launch!
+		        //this.cordova.setActivityResultCallback(null);
+		        this.cordova.startActivityForResult(this, intent, RINGTONE_PICKED);
+		        callbackContext.success();
+		    	
+		        return true;
 		    }
 		    callbackContext.error("Invalid action");
 		    return false;
@@ -30,4 +63,19 @@ public class ringtone extends CordovaPlugin {
 		    return false;
 		} 
 	}
+	
+	@Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		System.err.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>onActivityResult>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+		Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+		if (uri != null)
+		{
+		   System.err.println(">>>>>>>>>>>>>>>>>>>>RINGTONE: " + uri);
+           RingtoneManager.setActualDefaultRingtoneUri(this.cordova.getActivity().getBaseContext(), RingtoneManager.TYPE_RINGTONE, uri);
+           Settings.System.putString(this.cordova.getActivity().getContentResolver(), Settings.System.NOTIFICATION_SOUND, uri.toString());
+           
+		}
+    }
+	
 }
--- www/Ringtone.js
+++ www/Ringtone.js
@@ -1,17 +1,11 @@
 var calendar =  {
-    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    createEvent: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'ringtone', // mapped to our native Java class called "Calendar"
             'selectRingtone', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
-            }]
+            []
         );
     }
 }
